9ad280485887dd8cc6b72b84aad0a3a51c0c256f
David Ebukali <debukali@gmail.com>
2019-04-10 06:06:23 +0300

    Add unit tests for algebra expressions

--- js/controllers/WebDevController.js
+++ js/controllers/WebDevController.js
@@ -1,15 +1,19 @@
 webDevApp.controller('WebDevController', function WebDevController(
     $scope,
     Maths,
-    TableRows) {
+    TableRows,
+    $q) {
     var vm = $scope;
     vm.mathData = {
+        numberOne: "",
+        numberTwo: "",
         operation: "add"
     };
 
     vm.post = function (event) {
-        var numberOne = document.getElementsByName("numberOne")[0].value,
-            numberTwo = document.getElementsByName("numberTwo")[0].value;
+        var defer = $q.defer();
+        var numberOne = vm.mathData.numberOne,
+            numberTwo = vm.mathData.numberTwo;
         if(numberOne.length == 0 || numberTwo.length == 0) {
             alert("Both number one and two are required");
         }else {
@@ -19,10 +23,11 @@ webDevApp.controller('WebDevController', function WebDevController(
                         var clientResult = parseInt(numberOne) + parseInt(numberTwo);
                         TableRows.setData(setUpRowData(numberOne, numberTwo, clientResult, res.data.result));
                         $scope.$broadcast("mathResult");
-    
+                        defer.resolve(res.data);
                     }, function (err) {
                         console.log("Error Didnot Add " + JSON.stringify(err));
                         alert("Error Didnot Add "+JSON.stringify(err));
+                        defer.reject();
                     });
                     break;
                 case 'sub':
@@ -30,10 +35,11 @@ webDevApp.controller('WebDevController', function WebDevController(
                         var clientResult = parseInt(numberOne) - parseInt(numberTwo);
                         TableRows.setData(setUpRowData(numberOne, numberTwo, clientResult, res.data.result));
                         $scope.$broadcast("mathResult");
-    
+                        defer.resolve(res.data);
                     }, function (err) {
                         console.log("Error Didnot Subtract " + JSON.stringify(err));
                         alert("Error Didnot Subtract "+JSON.stringify(err));
+                        defer.reject();
                     });
                     break;
                 case 'mul':
@@ -41,10 +47,11 @@ webDevApp.controller('WebDevController', function WebDevController(
                         var clientResult = parseInt(numberOne) * parseInt(numberTwo);
                         TableRows.setData(setUpRowData(numberOne, numberTwo, clientResult, res.data.result));
                         $scope.$broadcast("mathResult");
-    
+                        defer.resolve(res.data);
                     }, function (err) {
                         console.log("Error Didnot Multiply " + JSON.stringify(err));
                         alert("Error Didnot Multiply "+JSON.stringify(err));
+                        defer.reject();
                     });
                     break;
                 case 'div':
@@ -52,25 +59,26 @@ webDevApp.controller('WebDevController', function WebDevController(
                         var clientResult = parseInt(numberOne) / parseInt(numberTwo);
                         TableRows.setData(setUpRowData(numberOne, numberTwo, clientResult, res.data.result));
                         $scope.$broadcast("mathResult");
-    
+                        defer.resolve(res.data);
                     }, function (err) {
                         console.log("Error Didnot Divide " + JSON.stringify(err));
                         alert("Error Didnot Divide "+JSON.stringify(err));
+                        defer.reject();
                     });
                     break;
                 default:
                     break;
             }
         }
+        return defer.promise;
     }
 
     function setUpRowData(numberOne, numberTwo, clientResult, apiResult) {
-        var select = document.getElementById("operation"),
-            apiResult = modifyApiResult(apiResult);
+        var apiResult = modifyApiResult(apiResult);
         return {
             numberOne: numberOne,
             numberTwo: numberTwo,
-            operation: select.options[select.selectedIndex].value,
+            operation: vm.mathData.operation,
             clientResult: clientResult,
             apiResult: apiResult,
             passed: clientResult == apiResult ? 'Yes' : 'No'
--- js/controllers/WebDevController.spec.js
+++ js/controllers/WebDevController.spec.js
@@ -1,35 +1,119 @@
 describe('WebDevController', function () {
     beforeEach(module('webDev'));
 
-    var controller, scope, webDevController;
+    var $httpBackend,
+        promise,
+        successCallback,
+        errorCallback,
+        validationErrorCallback,
+        validationSuccessCallback,
+        httpController,
+        $rootScope,
+        createController,
+        mathsUrl = 'http://api.mathjs.org/v4/',
+        $Lo;
 
-    beforeEach(inject(function(_$controller_, _$rootScope_){
-        // The injector unwraps the underscores (_) from around the parameter names when matching
-        $controller = _$controller_;
-        $rootScope = _$rootScope_;
+    beforeEach(inject(function ($injector, Lo) {
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        successCallback = jasmine.createSpy('SuccessMathOperation');
+        errorCallback = jasmine.createSpy('ErrorMathOperation');
 
-        scope = $rootScope.$new();
-        webDevController = $controller('WebDevController', { 
-            '$rootScope' : $rootScope,
-            $scope: scope 
-        });
+        $Lo = Lo;
+        var $controller = $injector.get('$controller');
+        createController = function () {
+            return $controller('WebDevController', { '$scope': $rootScope });
+        };
     }));
 
-    it('should exist', function() {
-        expect(webDevController).toBeDefined();
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
     });
 
-    // A set of tests for our .post() method
-    describe('.post()', function () {
-        it('should exist', function () {
-            expect(scope.post).toBeDefined();
-        });
+    it('Should exist', function () {
+        var httpController = createController();
+        expect(httpController).toBeDefined();
+    });
 
-        it('should add two numbers 2 + 2', function () {
-            //scope.mathData.operation = 'add';
-            
-            expect(scope.post).toBeDefined();
-        });
-    })
+    it('Adds two numbers 2 + 2', function (done) {
+        var data = {"result":"4","error":null};
+        $httpBackend.expectPOST(mathsUrl).respond(data);
+        var httpController = createController();
+        
+        $rootScope.mathData.operation = 'add';        
+        $rootScope.mathData.numberOne = 2;    
+        $rootScope.mathData.numberTwo = 2;    
+        promise = $rootScope.post();
+        promise.then(successCallback, errorCallback);
+    
+        $httpBackend.flush();
+    
+        expect(successCallback).toHaveBeenCalledWith(angular.fromJson(data));
+        expect(errorCallback).not.toHaveBeenCalled();
+    
+        $rootScope.$digest();
+        done();
+      });
+
+      it('Subtracts two numbers 4 - 2', function (done) {
+        var data = {"result":"2","error":null};
+        $httpBackend.expectPOST(mathsUrl).respond(data);
+        var httpController = createController();
+        
+        $rootScope.mathData.operation = 'sub';        
+        $rootScope.mathData.numberOne = 4;    
+        $rootScope.mathData.numberTwo = 2;    
+        promise = $rootScope.post();
+        promise.then(successCallback, errorCallback);
+    
+        $httpBackend.flush();
+    
+        expect(successCallback).toHaveBeenCalledWith(angular.fromJson(data));
+        expect(errorCallback).not.toHaveBeenCalled();
+    
+        $rootScope.$digest();
+        done();
+      });
+
+      it('Multiplies two numbers 3 * 5', function (done) {
+        var data = {"result":"15","error":null};
+        $httpBackend.expectPOST(mathsUrl).respond(data);
+        var httpController = createController();
+        
+        $rootScope.mathData.operation = 'mul';        
+        $rootScope.mathData.numberOne = 3;    
+        $rootScope.mathData.numberTwo = 5;    
+        promise = $rootScope.post();
+        promise.then(successCallback, errorCallback);
+    
+        $httpBackend.flush();
+    
+        expect(successCallback).toHaveBeenCalledWith(angular.fromJson(data));
+        expect(errorCallback).not.toHaveBeenCalled();
+    
+        $rootScope.$digest();
+        done();
+      });
+
+      it('Divides two numbers 12 / 6', function (done) {
+        var data = {"result":"2","error":null};
+        $httpBackend.expectPOST(mathsUrl).respond(data);
+        var httpController = createController();
+        
+        $rootScope.mathData.operation = 'div';        
+        $rootScope.mathData.numberOne = 12;    
+        $rootScope.mathData.numberTwo = 6;    
+        promise = $rootScope.post();
+        promise.then(successCallback, errorCallback);
+    
+        $httpBackend.flush();
+    
+        expect(successCallback).toHaveBeenCalledWith(angular.fromJson(data));
+        expect(errorCallback).not.toHaveBeenCalled();
+    
+        $rootScope.$digest();
+        done();
+      });
 
 });
